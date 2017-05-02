4bb7468838fcf5bd99d87b4541a18e315690dec5
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2017-05-02 21:19:04 +0200

    Add TestProject test_boost_XMLLoad

---
+++ src/persistence/_tmp/test_boost_XMLLoad/src/myTestBoostXMLLoad.cpp
@@ -0,0 +1,21 @@
+/*
+ * myTestBoostXMLLoad.cpp
+ *
+ *  Created on: 02.05.2017
+ *      Author: Alexander
+ */
+
+#include "myTestBoostXMLLoad.hpp"
+
+namespace testBoostXMLLoad {
+
+myTestBoostXMLLoad::myTestBoostXMLLoad() {
+	// TODO Auto-generated constructor stub
+
+}
+
+myTestBoostXMLLoad::~myTestBoostXMLLoad() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace testBoostXMLLoad */
---
+++ src/persistence/_tmp/test_boost_XMLLoad/src/myTestBoostXMLLoad.hpp
@@ -0,0 +1,23 @@
+/*
+ * myTestBoostXMLLoad.hpp
+ *
+ *  Created on: 02.05.2017
+ *      Author: Alexander
+ */
+
+#ifndef MYTESTBOOSTXMLLOAD_HPP_
+#define MYTESTBOOSTXMLLOAD_HPP_
+
+#include <boost/>
+
+namespace testBoostXMLLoad {
+
+class myTestBoostXMLLoad {
+public:
+	myTestBoostXMLLoad();
+	virtual ~myTestBoostXMLLoad();
+};
+
+} /* namespace testBoostXMLLoad */
+
+#endif /* MYTESTBOOSTXMLLOAD_HPP_ */
