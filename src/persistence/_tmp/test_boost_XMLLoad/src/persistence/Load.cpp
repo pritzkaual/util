e22671992ed3ff82356cfaebf1eb1267c6c8eba5
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2017-06-01 10:45:34 +0200

    Add interface class Load to persistence.

---
+++ src/persistence/_tmp/test_boost_XMLLoad/src/persistence/Load.cpp
@@ -0,0 +1,21 @@
+/*
+ * Load.cpp
+ *
+ *  Created on: 29.05.2017
+ *      Author: Alexander
+ */
+
+#include "Load.hpp"
+
+namespace persistence {
+
+Load::Load () {
+	// TODO Auto-generated constructor stub
+
+}
+
+Load::~Load () {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace persistence */
---
+++ src/persistence/_tmp/test_boost_XMLLoad/src/persistence/Load.hpp
@@ -0,0 +1,24 @@
+/*
+ * Load.hpp
+ *
+ *  Created on: 29.05.2017
+ *      Author: Alexander
+ */
+
+#ifndef LOAD_HPP_
+#define LOAD_HPP_
+
+#include "ExtendedMetaData.hpp"
+#include "Handler.hpp"
+
+namespace persistence {
+
+class Load {
+public:
+	Load ();
+	virtual ~Load ();
+};
+
+} /* namespace persistence */
+
+#endif /* LOAD_HPP_ */
