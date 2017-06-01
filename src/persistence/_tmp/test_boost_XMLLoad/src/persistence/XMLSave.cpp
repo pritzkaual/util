846a36345a54ec5b4ab54e31d38b1cdbcc88ec2a
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2017-06-01 10:55:56 +0200

    Fixed using of Interface-Class. Removed API and Impl of save(). Add and implement private API write(). Fixed member attribute.

--- src/persistence/_tmp/test_boost_XMLLoad/src/persistence/XMLSave.cpp
+++ src/persistence/_tmp/test_boost_XMLLoad/src/persistence/XMLSave.cpp
@@ -6,16 +6,30 @@
  */
 
 #include "XMLSave.hpp"
+//#include "ExtendedMetaData.hpp"
 
 namespace persistence {
 
+const std::string XMLSave::wasd = ExtendedMetaData::XMI_PREFIX; // TODO Remove this.
+
 XMLSave::XMLSave () {
 	// TODO Auto-generated constructor stub
-
 }
 
 XMLSave::~XMLSave () {
 	// TODO Auto-generated destructor stub
+}
+/*
+bool XMLSave::save ( const std::string& filename, std::shared_ptr<ecore::EObject> model, std::set<std::string> options ) {
+
+	return write(filename, options);
+}
+*/
+bool XMLSave::write (const std::string &filename ) {
+
+	boost::property_tree::write_xml( filename, m_tree, std::locale(), boost::property_tree::xml_writer_make_settings<std::string>( ' ', 2 ) );
+
+	return true;
 }
 
 } /* namespace persistence */
--- src/persistence/_tmp/test_boost_XMLLoad/src/persistence/XMLSave.hpp
+++ src/persistence/_tmp/test_boost_XMLLoad/src/persistence/XMLSave.hpp
@@ -8,21 +8,27 @@
 #ifndef XMLSAVE_HPP_
 #define XMLSAVE_HPP_
 
-#include "ExtendedMetaData.hpp"
-#include <map>
+#include "Save.hpp"
+#include "boost/property_tree/xml_parser.hpp"
+
+//#include "ExtendedMetaData.hpp"
+
 #include <string>
+
 
 namespace persistence {
 
-class XMLSave {
+class XMLSave : public Save {
 public:
 	XMLSave ();
 	virtual ~XMLSave ();
 
-	void save ( const std::string& filename, ecore::EObject* model, std::map<boost::any, boost::any> options );
+	//bool save ( const std::string &filename, std::shared_ptr<ecore::EObject> model, std::set<std::string> options );
 
 private:
-	const std::string wasd = ExtendedMetaData::XMI_PREFIX; // TODO Remove this.
+	static const std::string wasd; // TODO Remove this.
+
+	bool write (const std::string &filename);
 
 };
 
