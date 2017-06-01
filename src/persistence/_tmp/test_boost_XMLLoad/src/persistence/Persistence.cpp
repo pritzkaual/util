bb3e6353067459a3aea63bc957ce416fce6306dd
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2017-06-01 10:45:34 +0200

    Append include Headers. Add and implement APIs load() and save(). Add API isValidFile(). Add some member attributes.

--- src/persistence/_tmp/test_boost_XMLLoad/src/persistence/Persistence.cpp
+++ src/persistence/_tmp/test_boost_XMLLoad/src/persistence/Persistence.cpp
@@ -7,15 +7,51 @@
 
 #include "Persistence.hpp"
 
+using namespace std;
+
 namespace persistence {
+
+Persistence::Persistence () {
+
+}
 
 Persistence::~Persistence () {
 	// TODO Auto-generated destructor stub
 }
 
-Persistence::Persistence () {
-	// TODO Auto-generated constructor stub
+bool Persistence::load ( const std::string& filename, std::shared_ptr<ecore::EObject> model, std::set<std::string> options ) {
+	isValidFile( filename );
+	return true;
+}
 
+bool Persistence::save ( const std::string& filename, std::shared_ptr<ecore::EObject> model, std::set<std::string> options ) {
+	if ( !isValidFile( filename ) ) {
+		std::cout << "| ERROR    | " << "Given filename: '" << filename <<"' is not valid!" << std::endl;
+	}
+
+
+	if ( options.find( persistence::Option::OPTION_SAVE_JSON ) != options.end() ) {
+		std::cout << "| INFO     | " << "Save as JSON-File" << "\n";
+
+		std::shared_ptr<persistence::JSONSave> tmp_save = std::make_shared<persistence::JSONSave>();
+		tmp_save->save(filename, model, options); // TODO use inheritance method in JSONSave
+	}
+	else {
+		std::cout << "| INFO     | " << "Save as XML-File" << "\n";
+
+		std::shared_ptr<persistence::XMLSave> tmp_save = std::make_shared<persistence::XMLSave>();
+		tmp_save->save(filename, model, options); // TODO use inheritance method in XMLSave
+	}
+
+
+
+	return true;
+}
+
+bool Persistence::isValidFile ( const std::string& filename ) {
+	//boost::filesystem::path file( filename );
+
+	return true;
 }
 
 } /* namespace persistence */
--- src/persistence/_tmp/test_boost_XMLLoad/src/persistence/Persistence.hpp
+++ src/persistence/_tmp/test_boost_XMLLoad/src/persistence/Persistence.hpp
@@ -8,15 +8,43 @@
 #ifndef PERSISTENCE_HPP_
 #define PERSISTENCE_HPP_
 
-#include "XMLLoad.hpp"
+
+#include "Load.hpp"
+#include "Save.hpp"
+#include "JSONSave.hpp"
 #include "XMLSave.hpp"
+
+#include "Option.hpp"
+
+#include "EObject.hpp"
+
+//#include "boost/filesystem.hpp"   // includes all needed Boost.Filesystem declarations
+#include <string>
+#include <set>
+#include <iostream>
+
 
 namespace persistence {
 
 class Persistence {
 public:
-	virtual ~Persistence ();
 	Persistence ();
+	virtual ~Persistence ();
+
+	bool load(const std::string &filename, std::shared_ptr<ecore::EObject> model, std::set<std::string> options);
+	bool save(const std::string &filename, std::shared_ptr<ecore::EObject> model, std::set<std::string> options);
+
+private:
+	bool isValidFile(const std::string &filename);
+
+	//boost::filesystem::path m_file;
+	std::shared_ptr<ecore::EObject> m_model;
+	std::shared_ptr<ecore::EObject> m_metamodel;
+	std::shared_ptr<std::set<std::string>> m_options;
+
+
+	std::shared_ptr<persistence::Load> m_load;
+	std::shared_ptr<persistence::Save> m_save;
 };
 
 } /* namespace persistence */
