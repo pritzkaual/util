24f90d6c4f491f727e978618a1bffd87cdbaca6e
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2017-06-29 08:44:04 +0200

    Create Class TestModel. Moved createEcoreTestMetaModel() from main_myTestBoostXMLLoad.cpp to Class TestModel. Append use of Xerces-C

---
+++ src/persistence/_tmp/test_boost_XMLLoad/src/TestModel.hpp
@@ -0,0 +1,28 @@
+/*
+ * TestModel.hpp
+ *
+ *  Created on: 23.06.2017
+ *      Author: Alexander
+ */
+
+#ifndef _TESTMODEL_HPP_
+#define _TESTMODEL_HPP_
+
+#include "EcoreFactory.hpp"
+#include "EcorePackage.hpp"
+#include "impl/EcoreFactoryImpl.hpp"
+
+
+namespace testmodel {
+
+class TestModel {
+public:
+	TestModel ();
+	virtual ~TestModel ();
+
+	static std::shared_ptr<ecore::EObject> createEcoreTestMetaModel ();
+};
+
+} /* namespace persistence */
+
+#endif /* TESTMODEL_HPP_ */
