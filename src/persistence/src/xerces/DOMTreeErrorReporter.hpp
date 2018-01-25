0fe7e8a5aff0366e9039ddc5da76a485bd661eb8
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2018-01-25 15:33:19 +0100

    Fixed using W() instead of StrX().

--- src/persistence/src/xerces/DOMTreeErrorReporter.hpp
+++ src/persistence/src/xerces/DOMTreeErrorReporter.hpp
@@ -29,6 +29,7 @@
 #include <iostream.h>
 #endif
 
+#include "WStr.hpp"
 
 XERCES_CPP_NAMESPACE_USE
 
@@ -76,52 +77,6 @@ public:
 inline bool DOMTreeErrorReporter::getSawErrors() const
 {
     return fSawErrors;
-}
-
-// ---------------------------------------------------------------------------
-//  This is a simple class that lets us do easy (though not terribly efficient)
-//  trancoding of XMLCh data to local code page for display.
-// ---------------------------------------------------------------------------
-class StrX
-{
-public :
-    // -----------------------------------------------------------------------
-    //  Constructors and Destructor
-    // -----------------------------------------------------------------------
-    StrX(const XMLCh* const toTranscode)
-    {
-        // Call the private transcoding method
-        fLocalForm = XMLString::transcode(toTranscode);
-    }
-
-    ~StrX()
-    {
-        XMLString::release(&fLocalForm);
-    }
-
-
-    // -----------------------------------------------------------------------
-    //  Getter methods
-    // -----------------------------------------------------------------------
-    const char* localForm() const
-    {
-        return fLocalForm;
-    }
-
-private :
-    // -----------------------------------------------------------------------
-    //  Private data members
-    //
-    //  fLocalForm
-    //      This is the local code page form of the string.
-    // -----------------------------------------------------------------------
-    char*   fLocalForm;
-};
-
-inline std::ostream& operator<<(std::ostream& target, const StrX& toDump)
-{
-    target << toDump.localForm();
-    return target;
 }
 
 #endif /* DOM_TREE_ERROR_REPORTER_HPP_ */
