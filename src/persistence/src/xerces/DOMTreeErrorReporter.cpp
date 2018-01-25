7b723a16eeb512459cf9b159ebc839014a3608f3
Alexander Pritzkau <alexander.pritzkau@tu-ilmenau.de>
2018-01-25 15:41:10 +0100

    Fixed using W() instead of StrX().

--- src/persistence/src/xerces/DOMTreeErrorReporter.cpp
+++ src/persistence/src/xerces/DOMTreeErrorReporter.cpp
@@ -43,19 +43,19 @@ void DOMTreeErrorReporter::warning(const SAXParseException&)
 void DOMTreeErrorReporter::error(const SAXParseException& toCatch)
 {
     fSawErrors = true;
-    XERCES_STD_QUALIFIER cerr << "Error at file \"" << StrX(toCatch.getSystemId())
+    XERCES_STD_QUALIFIER cerr << "Error at file \"" << W(toCatch.getSystemId())
 		 << "\", line " << toCatch.getLineNumber()
 		 << ", column " << toCatch.getColumnNumber()
-         << "\n   Message: " << StrX(toCatch.getMessage()) << XERCES_STD_QUALIFIER endl;
+         << "\n   Message: " << W(toCatch.getMessage()) << XERCES_STD_QUALIFIER endl;
 }
 
 void DOMTreeErrorReporter::fatalError(const SAXParseException& toCatch)
 {
     fSawErrors = true;
-    XERCES_STD_QUALIFIER cerr << "Fatal Error at file \"" << StrX(toCatch.getSystemId())
+    XERCES_STD_QUALIFIER cerr << "Fatal Error at file \"" << W(toCatch.getSystemId())
 		 << "\", line " << toCatch.getLineNumber()
 		 << ", column " << toCatch.getColumnNumber()
-         << "\n   Message: " << StrX(toCatch.getMessage()) << XERCES_STD_QUALIFIER endl;
+         << "\n   Message: " << W(toCatch.getMessage()) << XERCES_STD_QUALIFIER endl;
 }
 
 void DOMTreeErrorReporter::resetErrors()
